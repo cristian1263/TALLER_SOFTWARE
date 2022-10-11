dfb0e365016ae3f06681b33f590628bd0a03ca7c
Cristian <cristian.camilo1560@gmail.com>
2022-10-10 21:26:55 -0500

    Segundo Punto

--- funciones.js
+++ funciones.js
@@ -36,11 +36,98 @@ if(Nfinal == "1" || Nfinal == "2")
       console.log("Ingrese solo numeros para la placa");
      }
   }
-  
+  //Mostramos los resultados
   console.log("El total de carros con calcomanias amarillas son:"+Amarillo+" Rosa:"+Rosa+" Rojos:"+Roja+" Verde:"+Verde+" Azules:"+Azul);
 
 }
 colorcal();
 
+function zoo()
+{
+  //pedimos el nombre del animal
+  NombreAnimal=prompt('Ingrese el nombre del animal "Jirafa, Elefante, Mono"');
+  //creamos los contadores y porcentajes para guardar los datos
+  contadoredad1=0; contadoredad2=0; contadoredad3=0;
+  porcentajedad1=0; porcentajedad2=0; porcentajedad3=0;
+ 
+  if("Jirafa" == NombreAnimal)
+ {
+  //Creamos el for a base del enunciado
+  for (i=1;i<=15;i++)
+  {
+    //Se pide la edad del animal
+    Edad= parseInt(prompt('Ingrese el la edad del animal'));
+     //Los clasificamos por la edad y contamos cuantas veces se clasifica en cada parte
+    if(Edad == 0 || Edad == 1)
+    {
+      contadoredad1=contadoredad1+1;
+    }else if(Edad > 1 && Edad <= 3){
+      contadoredad2=contadoredad2+1;
+    }else if(Edad > 3){
+      contadoredad3=contadoredad3+1;
+    }else{
+      console.log("Ingrese solo numeros");
+    }
+  }
+   //tomamos los datos finales de cada contador y se saca su porcentaje para mostrarlo
+   porcentajedad1=((contadoredad1*100)/15);
+   porcentajedad2=((contadoredad2*100)/15);
+   porcentajedad3=((contadoredad3*100)/15);
+   Porcentajetotal=porcentajedad1+porcentajedad2+porcentajedad3;
+   console.log("El porcentaje de edad de Jirafas de 0 a 1 año es:"+porcentajedad1+" De 1 a 3:"+porcentajedad2+" mayor a 3:"+porcentajedad3);
+   console.log("Porcentaje total:"+Porcentajetotal);
 
+ }else if("Elefante" == NombreAnimal)
+  {
+   for (i=1;i<=20;i++)
+   {
+    Edad= parseInt(prompt('Ingrese el la edad del animal'));
+
+    if(Edad == 0 || Edad == 1)
+    {
+      contadoredad1=contadoredad1+1;
+    }else if(Edad > 1 && Edad <= 3){
+      contadoredad2=contadoredad2+1;
+    }else if(Edad > 3 || Edad <= 0){
+      contadoredad3=contadoredad3+1;
+    }else{
+      console.log("Ingrese solo numeros");
+    }
+   }
+   porcentajedad1=((contadoredad1*100)/20);
+   porcentajedad2=((contadoredad2*100)/20);
+   porcentajedad3=((contadoredad3*100)/20);
+   Porcentajetotal=porcentajedad1+porcentajedad2+porcentajedad3;
+   console.log("El porcentaje de edad de Elefantes de 0 a 1 año es:"+porcentajedad1+" De 1 a 3:"+porcentajedad2+" mayor a 3:"+porcentajedad3);
+   console.log("Porcentaje total:"+Porcentajetotal);
+
+  }else if("Mono" == NombreAnimal)
+   { 
+    for (i=1;i<=40;i++)
+    {
+      Edad= parseInt(prompt('Ingrese el la edad del animal'));
+
+      if(Edad == 0 || Edad == 1)
+      {
+       contadoredad1=contadoredad1+1;
+      }else if(Edad > 1 && Edad <= 3){
+       contadoredad2=contadoredad2+1;
+      }else if(Edad > 3 ){
+      contadoredad3=contadoredad3+1;
+      }else{
+        console.log("Ingrese solo numeros");
+      }
+    }
+    porcentajedad1=((contadoredad1*100)/40);
+    porcentajedad2=((contadoredad2*100)/40);
+    porcentajedad3=((contadoredad3*100)/40);
+    Porcentajetotal=porcentajedad1+porcentajedad2+porcentajedad3;
+    console.log("El porcentaje de edad de Monos de 0 a 1 año es:"+porcentajedad1+" De 1 a 3:"+porcentajedad2+" mayor a 3:"+porcentajedad3);
+    console.log("Porcentaje total:"+Porcentajetotal);
+
+   }else{
+     console.log("Ingrese correctamente el nombre del animal ha realizar la muestra");
+    }
+}
+zoo();
 
